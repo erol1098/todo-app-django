7d1b52602a113ba85eedf7510f4d7d235b24b785
erol1098 <erol1098@gmail.com>
2022-08-29 21:26:22 +0300

    Add todo

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from todo.forms import TodoForm
 
 from todo.models import Todo
 
@@ -9,3 +9,18 @@ def home(request):
     "todos":todos
   }
   return render(request, "todo/home.html", context)
+
+
+def todo_create(request):
+  form = TodoForm()
+
+  if request.method == "POST":
+    form = TodoForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect("home")
+      
+  context = {
+    "form":form
+  }
+  return render(request, "todo/todo_add.html", context)
