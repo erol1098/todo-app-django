c1678cc6ebd625d0c721dc7e9b9ca8a06f5839db
erol1098 <erol1098@gmail.com>
2022-09-03 12:19:31 +0300

    Config

--- main/settings.py
+++ main/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
